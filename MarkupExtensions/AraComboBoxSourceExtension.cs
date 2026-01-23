0ddd748439a85c7875617ba8f5b122672792301c
skuzmin <erngorn@gmail.com>
2026-01-23 13:36:48 +0200

    add markup extension

---
+++ MarkupExtensions/AraComboBoxSourceExtension.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Data;
+using System.Windows.Markup;
+using ARA.Controls;
+
+namespace ARA.MarkupExtensions
+{
+	[MarkupExtensionReturnType(typeof(RelativeSource))]
+	public class AraComboBoxSourceExtension : MarkupExtension
+	{
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return new RelativeSource(RelativeSourceMode.FindAncestor, typeof(AraComboBox), 1);
+		}
+	}
+}
