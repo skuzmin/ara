01f2e0eafdce501fe847a2dfe57dd6b91e101a45
skuzmin <erngorn@gmail.com>
2026-01-28 16:47:53 +0200

    refactoring

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,12 +5,30 @@ using Hardcodet.Wpf.TaskbarNotification;
 
 namespace ARA
 {
-    public partial class App : Application
-    {
+	public partial class App : Application
+	{
+		private static Mutex? _mutex = null;
 		protected override void OnStartup(StartupEventArgs e)
 		{
+            SingleInstanceChecker();
 			base.OnStartup(e);
+			TrayIconInit();
+			Mouse.OverrideCursor = CursorHelper.CreateCursorFromPng("pack://application:,,,/Assets/cursor.png", 0, 0);
+		}
 
+		private static void SingleInstanceChecker()
+		{
+			_mutex = new Mutex(true, "ARA_ArcRaidersAssistant", out bool isFirstInstance);
+			if (!isFirstInstance)
+			{
+				MessageBox.Show("Appllication is already running.", "[ARA] Arc Raiders Assistant", MessageBoxButton.OK, MessageBoxImage.Information);
+				Application.Current.Shutdown();
+				return;
+			}
+		}
+
+		private void TrayIconInit()
+		{
 			if (Resources["TrayIcon"] is TaskbarIcon trayIcon)
 			{
 				trayIcon.Visibility = System.Windows.Visibility.Visible;
@@ -21,9 +39,6 @@ namespace ARA
 					MainWindow.Activate();
 				};
 			}
-
-			Mouse.OverrideCursor = CursorHelper.CreateCursorFromPng("pack://application:,,,/Assets/cursor.png", 0, 0);
 		}
 	}
-
 }
