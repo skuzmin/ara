f3ffd96c8d95c22359918819b2ff9005868bcd38
skuzmin <erngorn@gmail.com>
2026-01-29 19:17:10 +0200

    add navbar animation

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,60 @@
-﻿using ARA.ViewModels.Shell;
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using ARA.Controls.CustomControls;
+using ARA.ViewModels.Shell;
 
 namespace ARA
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : AraWindow
 	{
+		public required AraButton ActiveButton;
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			DataContext = new MainViewModel();
+			Loaded += (s, e) => InitPillPosition();
+		}
+
+        private void Navigation_Click(object sender, RoutedEventArgs e)
+        {
+			var button = (AraButton)sender;
+			if(ActiveButton == button)
+			{
+				return;
+			}
+			ActiveButton = button;
+			
+			Point buttonPosition = button.TransformToAncestor(NavbarGrid).Transform(new Point(0, 0));
+
+			var positionAnimation = new DoubleAnimation
+			{
+				To = buttonPosition.X,
+				Duration = TimeSpan.FromMilliseconds(150),
+				EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+			};
+			var widthAnimation = new DoubleAnimation
+			{
+				To = button.ActualWidth,
+				Duration = TimeSpan.FromMilliseconds(150),
+				EasingFunction = new QuadraticEase { EasingMode = EasingMode.EaseInOut }
+			};
+
+			SelectionPill.RenderTransform.BeginAnimation(TranslateTransform.XProperty, positionAnimation);
+			SelectionPill.BeginAnimation(FrameworkElement.WidthProperty, widthAnimation);
+		}
+
+		private void InitPillPosition()
+		{
+			var selectedButton = NavbarGrid.Children.OfType<AraButton>().FirstOrDefault(b => b.IsSelected);
+			if (selectedButton != null) 
+			{
+				Point position = selectedButton.TransformToAncestor(NavbarGrid).Transform(new Point(0, 0));
+				SelectionPill.Width = selectedButton.ActualWidth;
+				SelectionPill.RenderTransform = new TranslateTransform(position.X, 0);
+				ActiveButton = selectedButton;
+			}
 		}
 	}
 }
