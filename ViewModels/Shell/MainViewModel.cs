ca82f547fa9d410b1cff258bfe0bdfa43a0eda9c
skuzmin <erngorn@gmail.com>
2026-02-03 18:59:03 +0200

    refactor routing

--- ViewModels/Shell/MainViewModel.cs
+++ ViewModels/Shell/MainViewModel.cs
@@ -6,6 +6,7 @@ namespace ARA.ViewModels.Shell
 {
 	public class MainViewModel : ViewModelBase
 	{
+		private readonly IServiceProvider _services;
 		private ViewModelBase? _currentPage;
 		public ViewModelBase? CurrentPage
 		{
@@ -19,21 +20,25 @@ namespace ARA.ViewModels.Shell
 				OnPropertyChanged(nameof(IsAboutSelected));
 			}
 		}
-
 		public ICommand ShowLoadoutCommand { get; }
 		public ICommand ShowSettingsCommand { get; }
 		public ICommand ShowAboutCommand { get; }
-
 		public bool IsLoadoutSelected => CurrentPage is LoadoutViewModel;
 		public bool IsSettingsSelected => CurrentPage is SettingsViewModel;
 		public bool IsAboutSelected => CurrentPage is AboutViewModel;
 
 		public MainViewModel(IServiceProvider services)
 		{
-			ShowLoadoutCommand = new RelayCommand(_ => CurrentPage = services.GetRequiredService<LoadoutViewModel>());
-			ShowSettingsCommand = new RelayCommand(_ => CurrentPage = services.GetRequiredService<SettingsViewModel>());
-			ShowAboutCommand = new RelayCommand(_ => CurrentPage = services.GetRequiredService<AboutViewModel>());
-			CurrentPage = services.GetRequiredService<LoadoutViewModel>();
+			_services = services;
+			ShowLoadoutCommand = new RelayCommand(_ => NavigateToPage<LoadoutViewModel>());
+			ShowSettingsCommand = new RelayCommand(_ => NavigateToPage<SettingsViewModel>());
+			ShowAboutCommand = new RelayCommand(_ => NavigateToPage<AboutViewModel>());
+			NavigateToPage<LoadoutViewModel>();
+		}
+
+		private void NavigateToPage<TViewModel>() where TViewModel : ViewModelBase
+		{
+			CurrentPage = _services.GetRequiredService<TViewModel>();
 		}
 	}
 }
