e4f6b63243051ee2f3fed12e51b359d8554d3302
skuzmin <erngorn@gmail.com>
2026-02-11 19:17:59 +0200

    add loadout configs title bar

--- ViewModels/Pages/LoadoutConfigsViewModel.cs
+++ ViewModels/Pages/LoadoutConfigsViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace ARA.ViewModels.Pages
+﻿using System.Windows.Input;
+using ARA.Enums;
+using ARA.Interfaces;
+
+namespace ARA.ViewModels.Pages
 {
 	public class LoadoutConfigsViewModel : ViewModelBase
 	{
+		public ICommand BackCommand { get; }
 
+		public LoadoutConfigsViewModel(IAraNavigation navigations)
+		{
+			BackCommand = new RelayCommand(_ => navigations.NavigateToPage(AraPage.Loadout));
+		}
 	}
 }
