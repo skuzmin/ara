cbbeb12b972d4a135bec823eb9bbb3b06975e39b
skuzmin <erngorn@gmail.com>
2026-02-20 18:39:39 +0200

    add and bind blank save config method

--- ViewModels/Pages/LoadoutConfigDetailsViewModel.cs
+++ ViewModels/Pages/LoadoutConfigDetailsViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.ObjectModel;
-using System.Diagnostics;
 using System.Windows.Input;
 using ARA.Enums;
 using ARA.Interfaces;
@@ -11,7 +10,9 @@ namespace ARA.ViewModels.Pages
 	public class LoadoutConfigDetailsViewModel : ViewModelBase
 	{
 		private readonly ILogger _logger;
+		private readonly IAraNavigation _navigation;
 		public ICommand BackCommand { get; }
+		public ICommand SaveLoadoutConfigurationCommand { get; }
 		public ObservableCollection<GameItem> ItemsList { get; }
 		public LoadoutConfiguration LoadoutConfiguration { get; }
 		public string Title { get; }
@@ -22,22 +23,27 @@ namespace ARA.ViewModels.Pages
 			{
 				if (value != null)
 				{
-
 					_logger.LogInformation("Change loadout to: {loadout}", value.Name);
-
 				}
 				field = value;
 				OnPropertyChanged(nameof(SelectedItem));
 			}
 		}
 
-		public LoadoutConfigDetailsViewModel(IAraNavigation navigations, IAraConfigurations configurations, ILogger logger)
+		public LoadoutConfigDetailsViewModel(IAraNavigation navigation, IAraConfigurations configurations, ILogger logger)
 		{
 			_logger = logger;
+			_navigation = navigation;
 			LoadoutConfiguration = configurations.GetCurrentLoadoutConfig();
 			Title = string.IsNullOrEmpty(LoadoutConfiguration.Name) ? "New Configuration" : LoadoutConfiguration.Name;
-			BackCommand = new RelayCommand(_ => navigations.NavigateToPage(AraPage.LoadoutConfigs));
+			BackCommand = new RelayCommand(_ => navigation.NavigateToPage(AraPage.LoadoutConfigs));
+			SaveLoadoutConfigurationCommand = new RelayCommand(_ => SaveLoadoutConfiguration());
 			ItemsList = new ObservableCollection<GameItem>(GameItem.GetList());
+		}
+
+		private void SaveLoadoutConfiguration()
+		{
+			//_navigation.NavigateToPage(AraPage.LoadoutConfigs);
 		}
 	}
 }
