20891eecdb3586d4814e4e30ed293863274f7191
skuzmin <erngorn@gmail.com>
2026-02-21 02:22:40 +0200

    add data grid ( items ) to the details page

--- ViewModels/Pages/LoadoutConfigDetailsViewModel.cs
+++ ViewModels/Pages/LoadoutConfigDetailsViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.ObjectModel;
-using System.Runtime.CompilerServices;
 using System.Windows.Input;
 using ARA.Enums;
 using ARA.Interfaces;
@@ -15,8 +14,11 @@ namespace ARA.ViewModels.Pages
 		private readonly IAraConfigurations _configurations;
 		private readonly Boolean _isNewLoadout;
 		public ICommand BackCommand { get; }
+		public ICommand AddItemCommand { get; }
+		public ICommand RemoveItemCommand { get; }
 		public ICommand SaveLoadoutConfigurationCommand { get; }
 		public ObservableCollection<GameItem> ItemsList { get; }
+		public ObservableCollection<GameItem> SelectedItemsList { get; }
 		public LoadoutConfiguration LoadoutConfiguration { get; }
 		public string Title { get; }
 		public GameItem? SelectedItem
@@ -24,15 +26,10 @@ namespace ARA.ViewModels.Pages
 			get => field;
 			set
 			{
-				if (value != null)
-				{
-					_logger.LogInformation("Change loadout to: {loadout}", value.Name);
-				}
 				field = value;
 				OnPropertyChanged(nameof(SelectedItem));
 			}
 		}
-
 		public LoadoutConfigDetailsViewModel(IAraNavigation navigation, IAraConfigurations configurations, ILogger logger)
 		{
 			_logger = logger;
@@ -44,13 +41,16 @@ namespace ARA.ViewModels.Pages
 			LoadoutConfiguration = loadoutConfig ?? new LoadoutConfiguration();
 			Title = _isNewLoadout ? "New Configuration" : LoadoutConfiguration.Name;
 			ItemsList = new ObservableCollection<GameItem>(GameItem.GetList());
-
+			SelectedItemsList = new ObservableCollection<GameItem>(LoadoutConfiguration.Items);
+			AddItemCommand = new RelayCommand(_ => AddItem());
+			RemoveItemCommand = new RelayCommand(id => RemoveItem((Guid)id));
 			BackCommand = new RelayCommand(_ => navigation.NavigateToPage(AraPage.LoadoutConfigs));
 			SaveLoadoutConfigurationCommand = new RelayCommand(_ => SaveLoadoutConfiguration());
 		}
 
 		private void SaveLoadoutConfiguration()
 		{
+			LoadoutConfiguration.Items = [.. SelectedItemsList];
 			if (_isNewLoadout)
 			{
 				_configurations.CreateLoadoutConfig(LoadoutConfiguration);
@@ -60,6 +60,17 @@ namespace ARA.ViewModels.Pages
 				_configurations.UpdateLoadoutConfig(LoadoutConfiguration);
 			}
 			_navigation.NavigateToPage(AraPage.LoadoutConfigs);
+		}
+
+		private void AddItem()
+		{
+			SelectedItemsList.Add(SelectedItem!);
+			SelectedItem = null;
+		}
+
+		private void RemoveItem(Guid Id)
+		{
+
 		}
 	}
 }
