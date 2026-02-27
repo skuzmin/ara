5315eaa6f9bc33bf90a40ffefbc9a267c0d80b08
skuzmin <erngorn@gmail.com>
2026-02-27 22:20:43 +0200

    add about page, icons

--- ViewModels/Pages/AboutViewModel.cs
+++ ViewModels/Pages/AboutViewModel.cs
@@ -2,15 +2,5 @@
 {
 	public class AboutViewModel : ViewModelBase
 	{
-		private string _title = "About";
-		public string Title
-		{
-			get => _title;
-			set
-			{
-				_title = value;
-				OnPropertyChanged(nameof(Title));
-			}
-		}
 	}
 }
--- Views/AboutView.xaml.cs
+++ Views/AboutView.xaml.cs
@@ -1,12 +1,20 @@
-﻿using System.Windows.Controls;
+﻿using System.Diagnostics;
+using System.Windows.Controls;
 
 namespace ARA.Views
 {
-    public partial class AboutView : UserControl
-    {
-        public AboutView()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class AboutView : UserControl
+	{
+		public AboutView()
+		{
+			InitializeComponent();
+		}
+
+		private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+		{
+			var url = e.Uri.AbsoluteUri;
+			Process.Start(new ProcessStartInfo(url) { UseShellExecute = true });
+			e.Handled = true;
+		}
+	}
 }
